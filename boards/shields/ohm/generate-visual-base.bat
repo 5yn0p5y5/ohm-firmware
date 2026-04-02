651f24ec75d6b097d52aa792ea1ea37f275e9500
Bryce <brycekong@icloud.com>
2026-04-02 12:56:20 -0700

    finalize map generation workflow more

--- boards/shields/ohm/generate-visual-base.bat
+++ boards/shields/ohm/generate-visual-base.bat
@@ -1,3 +1,4 @@
 @echo off
 set PYTHONUTF8=1
-keymap -c draw_config.yaml draw ohm_keymap_base.yaml --dts-layout ohm-layouts.dtsi -o ohm_keymap_base.svg
+keymap -c draw_config_base.yaml parse -z ohm.keymap -c 10 -o ohm_keymap.yaml
+keymap -c draw_config_base.yaml draw ohm_keymap.yaml --dts-layout ohm-layouts.dtsi --select-layers base -o ohm_keymap_base.svg
--- boards/shields/ohm/generate-visual.bat
+++ boards/shields/ohm/generate-visual.bat
@@ -1,4 +1,4 @@
 @echo off
 set PYTHONUTF8=1
-REM keymap -c draw_config.yaml parse -z ohm.keymap -c 10 -o ohm_keymap.yaml
+keymap -c draw_config.yaml parse -z ohm.keymap -c 10 -o ohm_keymap.yaml
 keymap -c draw_config.yaml draw ohm_keymap.yaml --dts-layout ohm-layouts.dtsi -o ohm_keymap.svg
